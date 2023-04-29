5cc96fa57834da08302253856b721e8d8eb5f3e8
Rashid Abder Rahim <rabder24@gmail.com>
2023-04-28 17:25:12 -0700

    Test push

---
+++ helloworld.py
@@ -0,0 +1,20 @@
+import os 
+import openai
+
+
+openai.api_key = "YOUR-API-KEY-HERE"
+
+
+completion = openai.ChatCompletion.create(
+    model = "gpt-3.5-turbo",
+    messages=[
+        {
+            "role":"user",
+            "content": "Greet me in the style of a pirate"
+        }
+    ]
+)
+
+
+# prinnt the response
+print("\n"+completion.choices[0].message.content)
